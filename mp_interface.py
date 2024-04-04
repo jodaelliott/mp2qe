0952c8e14c426edad33875c4d2cd98849acfd693
jodaelliott <jodaelliott@gmail.com>
2024-04-04 16:16:26 +0100

    legacy update

--- mp_interface.py
+++ mp_interface.py
@@ -6,8 +6,10 @@
 import requests
 
 def get_url(m_id, key, feature='structure'):
-   url = "https://materialsproject.org/rest/v2/tasks/{m_id}/{feature}?API_KEY={key}"
+   url = "https://legacy.materialsproject.org/rest/v2/tasks/{m_id}/{feature}?API_KEY={key}"
    url = url.format(m_id = m_id, key = key, feature=feature)
+
+   print(url)
 
    return(url)
 
